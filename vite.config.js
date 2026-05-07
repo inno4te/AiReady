86d7cc93e1048f5955a81b5780a512cb5bb64d76
inno4te <forteh@gmail.com>
2026-05-07 22:54:32 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/AiReady/", // change to "/your-repo-name/" for GitHub Pages
+  base: "/", // change to "/your-repo-name/" for GitHub Pages
 });
